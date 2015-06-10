9c8c99037f0b991922414f5a3f7ec41b3d2ad249
Alexander Larsson <alexl@redhat.com>
2015-06-10 14:39:49 +0200

    build-app.sh updates

--- build-app.sh
+++ build-app.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+RPM=$1
+shift
+DIR=$1
+shift
+set -e
 set -x
 mock -r fedora-app-22-x86_64.cfg --clean
-mock -r fedora-app-22-x86_64.cfg --install --disablerepo=fedora --disablerepo=updates "$@"
-mock -r fedora-app-22-x86_64.cfg --shell "tar cvf /app.tar -C /app ."
+mock -r fedora-app-22-x86_64.cfg --install --disablerepo=fedora --disablerepo=updates "$RPM"
+mock -r fedora-app-22-x86_64.cfg --shell "tar cvf /app.tar /app"
 mock -r fedora-app-22-x86_64.cfg  --copyout /app.tar .
+
+xdg-app build $DIR tar xvCf / app.tar
+rm app.tar
